bde2a5c45e9a53840e1474ffd39993d184a01757
ravenmaps <65938320+ravenmaps@users.noreply.github.com>
2021-09-23 15:01:29 +1000

    Add files via upload

---
+++ data/lightrailstop_26.js
@@ -0,0 +1,19 @@
+var json_lightrailstop_26 = {
+"type": "FeatureCollection",
+"name": "lightrailstop_26",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "feature": "light rail stop", "desc_1": "LightRail1", "desc_2": null, "desc_3": null, "station": "Central", "fid": "801424d9-eaa9-4961-a079-e2d836d73912" }, "geometry": { "type": "Point", "coordinates": [ 151.206483878195002, -33.882240584980899 ] } },
+{ "type": "Feature", "properties": { "feature": "light rail stop", "desc_1": "LightRail2", "desc_2": null, "desc_3": null, "station": "Central", "fid": "84e7330e-c19b-4acb-a151-02b90acef7d1" }, "geometry": { "type": "Point", "coordinates": [ 151.207721047103007, -33.883981870090601 ] } },
+{ "type": "Feature", "properties": { "feature": "light rail stop", "desc_1": "LightRail3", "desc_2": null, "desc_3": null, "station": "Central", "fid": "089bc492-102c-44f4-a334-635ac53a2b72" }, "geometry": { "type": "Point", "coordinates": [ 151.207945011556006, -33.883912564504897 ] } },
+{ "type": "Feature", "properties": { "feature": "light rail stop", "desc_1": "LightRail1", "desc_2": null, "desc_3": null, "station": "CircularQuay", "fid": "d0915da4-5595-400c-9bee-83b0bc3e7ce9" }, "geometry": { "type": "Point", "coordinates": [ 151.209943, -33.86147 ] } },
+{ "type": "Feature", "properties": { "feature": "light rail stop", "desc_1": "LightRail2", "desc_2": null, "desc_3": null, "station": "CircularQuay", "fid": "aba99d70-33ec-415c-a82e-bccf8cd53494" }, "geometry": { "type": "Point", "coordinates": [ 151.209929, -33.861565 ] } },
+{ "type": "Feature", "properties": { "feature": "light rail stop", "desc_1": "LightRail1", "desc_2": null, "desc_3": null, "station": "DulwichHill", "fid": "b7b7c542-b1f7-4669-90dc-69a8bff0b9c3" }, "geometry": { "type": "Point", "coordinates": [ 151.140118, -33.910288 ] } },
+{ "type": "Feature", "properties": { "feature": "light rail stop", "desc_1": "LightRail1", "desc_2": null, "desc_3": null, "station": "NewcastleInterchange", "fid": "0d71fb87-6786-4f00-a1b5-43430f159da8" }, "geometry": { "type": "Point", "coordinates": [ 151.759677417576, -32.924348487829398 ] } },
+{ "type": "Feature", "properties": { "feature": "light rail stop", "desc_1": "LightRail1", "desc_2": null, "desc_3": null, "station": "TownHall", "fid": "9fdc97fb-30e5-442b-a967-1fbd301bfb2b" }, "geometry": { "type": "Point", "coordinates": [ 151.206853687762987, -33.873864328896303 ] } },
+{ "type": "Feature", "properties": { "feature": "light rail stop", "desc_1": "LightRail1", "desc_2": null, "desc_3": null, "station": "Wynyard", "fid": "b19c7146-0639-48f0-b882-571c1322e24c" }, "geometry": { "type": "Point", "coordinates": [ 151.20729692280301, -33.866067982537402 ] } },
+{ "type": "Feature", "properties": { "feature": "light rail stop", "desc_1": "LightRail2", "desc_2": null, "desc_3": null, "station": "Wynyard", "fid": "5192b917-8a65-4ccd-83bb-4599f87ac4ce" }, "geometry": { "type": "Point", "coordinates": [ 151.207175217568988, -33.8660624146572 ] } },
+{ "type": "Feature", "properties": { "feature": "light rail stop", "desc_1": "LightRail1", "desc_2": null, "desc_3": null, "station": "DarlingHarbour-ConventionCenterWharf", "fid": "3c64319b-2e3a-4a36-af07-89ab9cc280f8" }, "geometry": { "type": "Point", "coordinates": [ 151.198154613376005, -33.873081274501899 ] } },
+{ "type": "Feature", "properties": { "feature": "light rail stop", "desc_1": "LightRail1", "desc_2": null, "desc_3": null, "station": "QueensWharf", "fid": "a27d1303-3957-4bb9-a3ac-ad41be084133" }, "geometry": { "type": "Point", "coordinates": [ 151.781366094947003, -32.926318455456801 ] } }
+]
+}
